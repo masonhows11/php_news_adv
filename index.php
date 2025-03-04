f65b06ce60d0b1c1519fd367e393dd1cbb2bdce7
mason.hows11 <mason.hows11@gmail.com>
2025-03-04 17:17:25 +0330

    add mini rotuign system

--- index.php
+++ index.php
@@ -11,7 +11,8 @@ const BASE_PATH = __DIR__;
 
 
 // current domain
-define("CURRENT_DOMAIN", currentDomain() . '/php_news/');
+// define("CURRENT_DOMAIN", currentDomain() . '/php_news/');
+define("CURRENT_DOMAIN", currentDomain());
 const DISPLAY_ERROR = true;
 
 const DB_HOST = '127.0.0.1';
@@ -31,6 +32,11 @@ function currentDomain(): string
     return protocol() . $_SERVER['HTTP_HOST'];
 }
 
+function currentURL(): string
+{
+    return currentDomain() . $_SERVER['REQUEST_URI'];
+}
+
 
 function assets($path): string
 {
@@ -45,12 +51,6 @@ function url($path): string
     return $domain . '/' . trim($path . '/ .');
 
 }
-
-function currentURL(): string
-{
-    return currentDomain() . $_SERVER['REQUEST_URI'];
-}
-
 
 function methodType()
 {
@@ -78,31 +78,100 @@ displayError(DISPLAY_ERROR);
 
 
 global $flashMessage;
-if (isset($_SESSION['flash_message']))
-{
+if (isset($_SESSION['flash_message'])) {
     $flashMessage = $_SESSION['flash_message'];
     unset($_SESSION['flash_message']);
 
 }
 
 
-function flashMessage($name,$value = null)
+function flashMessage($name, $value = null)
 {
 
-    if($value === null){
+    if ($value === null) {
         global $flashMessage;
-        $message = isset($flashMessage[$name])  ?  $flashMessage[$name] : '';
+        $message = isset($flashMessage[$name]) ? $flashMessage[$name] : '';
         return $message;
-    }else{
+    } else {
+
         $_SESSION['flash_message'][$name] = $value;
+
     }
 
 }
 
-// flashMessage('test','test value');
-// echo  flashMessage('test');
+
+// mini routing system
+function uri($route = null, $controller = null, $method = null, $requestMethodType = 'GET')
+{
+
+    // current url array
+    $current_url = explode('?', currentURL())[0];
+    $current_url = str_replace(CURRENT_DOMAIN, ' ', $current_url);
+    $current_url = trim($current_url, '/ ');
+    $current_url_array = explode('/', $current_url);
+    $current_url_array = array_filter($current_url_array);
+
+    // reserved url array
+    $reservedUrl = trim($route, '/ ');
+    $reservedUrlArray = explode('/', $reservedUrl);
+    $reservedUrlArray = array_filter($reservedUrlArray);
+
+    // compare current_url_array & reservedUrlArray
+    if (sizeof($current_url_array) != sizeof($reservedUrlArray) || methodType() != $requestMethodType) {
+
+        return false;
+    }
+
+    // admin/category/create/{id} reserved
+    // admin/category/create/2 get from request
+    // find the parameter like id = 2
+
+    $parameters = [];
+    // find parameter with curly bracket in route
+    // like {id} {slug}
+    // for on each array item
+    for ($key = 0; $key < sizeof($current_url_array); $key++) {
+
+        // $reservedUrlArray[$key][strlen($reservedUrlArray[$key]) - 1] to get last character
+        if ($reservedUrlArray[$key][0] == "{" && $reservedUrlArray[$key][strlen($reservedUrlArray[$key]) - 1] == "}")
+        {
+
+          array_push( $parameters,$reservedUrlArray[$key]);
+
+          // compare each item route must be same
+        } elseif ($reservedUrlArray[$key] !== $current_url_array[$key]) {
+            return false;
+        }
+
+    }
 
 
+    if(methodType() == "POST"){
+
+        $request = isset($_FILES) ? array_merge($_POST,$_FILES) : $_POST;
+        $parameters = array_merge([$request],$parameters);
+    }
+
+    // create new obj from class/controller belongs to current route
+    // then execute
+    $object = new $controller;
+    call_user_func_array( array($object,$method),$parameters);
+    exit();
+
+
+}
+
+uri('admin/category/create', 'adminCategory', 'create');
+
+
+function dd($var)
+{
+
+    echo "<pre/>";
+    var_dump($var);
+    exit();
+}
 
 
 
