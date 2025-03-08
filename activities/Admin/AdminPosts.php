e191c229689b33045bf2a75e787380627f92a0dd
mason.hows11 <mason.hows11@gmail.com>
2025-03-08 15:42:53 +0330

    admin edit post with image updated 80%

--- activities/Admin/AdminPosts.php
+++ activities/Admin/AdminPosts.php
@@ -61,28 +61,26 @@ class AdminPosts extends AdminBase
         ////
         $db = new Database();
         ////
-        if ($request['categories_id'] != null)
-        {
+        if ($request['categories_id'] != null) {
 
             // if user upload new image
-            if($request['image']['tmp_name'] != null){
-                $post =  $db->select('SELECT * FROM posts WHERE id = ?;', [$id])->fetch();
+            if ($request['image']['tmp_name'] != null) {
+                $post = $db->select('SELECT * FROM posts WHERE id = ?;', [$id])->fetch();
                 $this->removeImage($post['image']);
                 $request['image'] = $this->saveImage($request['image'], 'post_image');
-            }
-            if ($request['image']) {
-
-                // Columns that are present in the query but not in the table
-                // will prevent the query from executing correctly.
-                $request = array_merge($request, ['user_id' => 1]);
-                $res = $db->update('posts', $id, array_keys($request), $request);
-                $this->redirect('admin/posts');
             } else {
-                $this->redirect('admin/posts');
+                unset($request['image']);
             }
+
         } else {
+            
             $this->redirect('admin/posts');
         }
+        // Columns that are present in the query but not in the table
+        // will prevent the query from executing correctly.
+        $request = array_merge($request, ['user_id' => 1]);
+        $res = $db->update('posts', $id, array_keys($request), $request);
+        $this->redirect('admin/posts');
     }
 
 
@@ -95,7 +93,7 @@ class AdminPosts extends AdminBase
     {
         $db = new Database();
         $post = $db->select('SELECT * FROM posts WHERE id = ?;', [$id])->fetch();
-        if($post['image'] != null){
+        if ($post['image'] != null) {
 
             $this->removeImage($post['image']);
         }
