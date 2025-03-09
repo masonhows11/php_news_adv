269444aa797865b9e322f777c34bff6fef8dee01
mason.hows11 <mason.hows11@gmail.com>
2025-03-09 18:52:19 +0330

    admin banners crud controller updated

--- activities/Admin/AdminBanners.php
+++ activities/Admin/AdminBanners.php
@@ -2,36 +2,51 @@
 
 namespace Admin;
 
+use database\Database;
+
+require "AdminBase.php";
 class AdminBanners
 {
     public function index()
     {
+        $db = new Database();
+        $banners = $db->select('SELECT * FROM banners ORDER BY `id` DESC ')->fetchAll();
         require_once (BASE_PATH.'/template/admin/banners/index.php');
     }
 
 
     public function create()
     {
-        echo "admin category create method";
+        require_once(BASE_PATH . '/template/admin/banners/create.php');
     }
 
-    public function store()
+    public function store($request)
     {
-        echo "admin category store method";
+        $db = new Database();
+        // Columns that are present in the query but not in the table
+        // will prevent the query from executing correctly.
+        $db->insert('banners', array_keys($request), $request);
+        $this->redirect('admin/category');
     }
 
-    public function edit()
+    public function edit($id)
     {
-        echo "admin category edit method";
+        $db = new Database();
+        $banner = $db->select('SELECT * FROM banners WHERE id = ?;',[$id])->fetch();
+        require_once(BASE_PATH . '/template/admin/banner/edit.php');
     }
 
-    public function update()
+    public function update($request,$id)
     {
-        echo "admin category update method";
+        $db = new Database();
+        $result = $db->update('banners',$id,array_keys($request),$request);
+        $this->redirect('admin/banners');
     }
 
-    public function delete()
+    public function delete($id)
     {
-        echo "admin category delete method";
+        $db = new Database();
+        $db->delete('categories',$id);
+        $this->redirect('admin/banners');
     }
 }
