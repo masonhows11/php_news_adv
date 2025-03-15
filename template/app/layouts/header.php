ca822927666f5b9707690ee9eed6cc394257944b
mason.hows11 <mason.hows11@gmail.com>
2025-03-15 16:31:53 +0330

    index implements updated

--- template/app/layouts/header.php
+++ template/app/layouts/header.php
@@ -1,1 +1,30 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Newsim HTML-5 Template</title>
+    <!-- favicon -->
+    <link rel="icon" href="../../public/front_assets/img/favicon.png" sizes="20x20" type="image/png" />
+    <!-- animate -->
+    <link rel="stylesheet" href="../../public/front_assets/css/animate.css" />
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="../../public/front_assets/css/bootstrap.min.css" />
+    <!-- magnific popup -->
+    <link rel="stylesheet" href="../../public/front_assets/css/magnific-popup.css" />
+    <!-- Nice Select -->
+    <link rel="stylesheet" href="../../public/front_assets/css/nice-select.css">
+    <!-- Slick Slider -->
+    <link rel="stylesheet" href="../../public/front_assets/css/slick.css">
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="../../public/front_assets/css/font-awesome.min.css" />
+    <!-- Flaticon -->
+    <link rel="stylesheet" href="../../public/front_assets/css/flaticon.css">
+    <!-- Main Stylesheet -->
+    <link rel="stylesheet" href="../../public/front_assets/css/style.css" />
+    <!-- Responsive Css -->
+    <link rel="stylesheet" href="../../public/front_assets/css/responsive.css">
+</head>
---
+++ template/app/layouts/navbar.php
@@ -0,0 +1,61 @@
+<nav class="navbar navbar-area navbar-expand-lg navigation-style-03">
+    <div class="container custom-container navi-container">
+        <div class="responsive-menu">
+            <div class="logo-wrapper">
+                <a href="index.html" class="logo">
+                    <img src="../../public/front_assets/img/logo/logo-white.svg" alt="" />
+                </a>
+            </div>
+            <button class="navbar-toggler navbar-bs-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#themeim_main_menu" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        <div class="collapse navbar-collapse" id="themeim_main_menu">
+            <ul class="navbar-nav">
+                <li class="menu-item-has-children current-menu-item">
+                    <a href="#">Home</a>
+                    <ul class="sub-menu">
+                        <li><a href="index.html">Home one</a></li>
+                        <li><a href="index-02.html">Home two</a></li>
+                        <li><a href="index-03.html">Home three</a></li>
+                    </ul>
+                </li>
+                <li><a href="business.html">Archive</a></li>
+                <li class="menu-item-has-children">
+                    <a href="#">Post Picks</a>
+                    <ul class="sub-menu">
+                        <li><a href="business.html">Business</a></li>
+                        <li><a href="business-02.html">Business two</a></li>
+                        <li><a href="sport.html">Sports</a></li>
+                    </ul>
+                </li>
+                <li><a href="financial.html">Financial Post</a></li>
+                <li class="menu-item-has-children">
+                    <a href="#">Pages</a>
+                    <ul class="sub-menu">
+                        <li><a href="about.html">About</a></li>
+                        <li><a href="author.html">Author</a></li>
+                        <li><a href="login.html">Log In</a></li>
+                        <li><a href="signup.html">Sing In</a></li>
+                    </ul>
+                </li>
+                <li class="menu-item-has-children">
+                    <a href="#">Shop</a>
+                    <ul class="sub-menu">
+                        <li><a href="products.html">Products</a></li>
+                        <li><a href="product-details.html">Products Details</a></li>
+                        <li><a href="cart.html">Cart</a></li>
+                    </ul>
+                </li>
+                <li><a href="contact.html">Contact</a></li>
+            </ul>
+        </div>
+        <div class="nav-right-content">
+            <div class="cart-icon open-cart">
+                <ul>
+                    <li><a href="javascript:void(0)"><i class="flaticon-trolley"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
