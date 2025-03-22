cf1a653a011ddb294e35ae2e1cadb7828d0b8c48
mason.hows11 <mason.hows11@gmail.com>
2025-03-22 22:44:30 +0330

    admin main view add second template

--- template/admin/layouts/home.php
+++ template/admin/layouts/home.php
@@ -34,7 +34,7 @@
                 <div class="col">
                     <div class="card" style="width: 18rem;">
                         <div class="card-body d-flex flex-column">
-                            <div class="d-flex justify-content-between"><h5 class="card-title"><i class="bi bi-boxes"></i> دسته بندی ها </h5><span><?= $category_count['COUNT(*)'] ?></span></div>
+                            <div class="d-flex justify-content-between bg-info-subtle p-3 my-2 rounded"><h5 class="card-title"><i class="bi bi-boxes"></i> دسته بندی ها </h5><span><?= $category_count['COUNT(*)'] ?></span></div>
                             <p class="card-text  p-2 mt-3"><a href="<?= url('admin/category') ?>" class="">نمایش دسته بندی ها</a></p>
                         </div>
                     </div>
@@ -42,30 +42,25 @@
                 <div class="col">
                     <div class="card" style="width: 18rem;">
                         <div class="card-body d-flex flex-column">
-                            <div class="d-flex justify-content-between"><h5 class="card-title"><i class="bi bi-people-fill"></i> کاربران </h5><span><?= $admins_count['COUNT(*)'] +  $users_count['COUNT(*)'] ?></span></div>
+                            <div class="d-flex justify-content-between bg-info-subtle p-3 my-2 rounded"><h5 class="card-title"><i class="bi bi-people-fill"></i> کاربران </h5><span><?= $admins_count['COUNT(*)'] +  $users_count['COUNT(*)'] ?></span></div>
                             <div class="d-flex justify-content-between"><h6 class="card-title"><i class="bi bi-people-fill"></i>  ادمین </h6><span><?= $admins_count['COUNT(*)'] ?></span></div>
                             <div class="d-flex justify-content-between"><h6 class="card-title"><i class="bi bi-people-fill"></i>  عادی </h6><span><?= $users_count['COUNT(*)'] ?></span></div>
                             <p class="card-text  p-2 mt-3"><a href="<?= url('admin/users') ?>" class="">لیست کاربران</a></p>
-
-
                         </div>
                     </div>
                 </div>
                 <div class="col">
                     <div class="card" style="width: 18rem;">
-
                         <div class="card-body d-flex flex-column">
-                            <div class="d-flex justify-content-between"><h5 class="card-title"><i class="bi bi-card-heading"></i> مقالات </h5><span><?= $posts_count['COUNT(*)'] ?></span></div>
+                            <div class="d-flex justify-content-between bg-info-subtle p-3 my-2 rounded"><h5 class="card-title"><i class="bi bi-card-heading"></i> مقالات </h5><span><?= $posts_count['COUNT(*)'] ?></span></div>
                             <p class="card-text  p-2 mt-3"><a href="<?= url('admin/posts') ?>" class="">مشاهده مقالات</a></p>
-
-
                         </div>
                     </div>
                 </div>
                 <div class="col">
                     <div class="card" style="width: 18rem;">
                         <div class="card-body d-flex flex-column">
-                            <div class="d-flex justify-content-between"><h5 class="card-title"><i class="bi bi-chat-text-fill"></i> دیدگاه ها </h5><span><?= $comments_count['COUNT(*)'] ?></span></div>
+                            <div class="d-flex justify-content-between bg-info-subtle p-3 my-2 rounded"><h5 class="card-title"><i class="bi bi-chat-text-fill"></i> دیدگاه ها </h5><span><?= $comments_count['COUNT(*)'] ?></span></div>
                             <div class="d-flex justify-content-between">
                                 <div><span><?= $approved_count['COUNT(*)'] ?></span> تایید شده </div>
                                 <div><span><?= $unseen_count['COUNT(*)'] ?></span> دیده نشده </div>
@@ -75,6 +70,70 @@
                         </div>
                     </div>
                 </div>
+
+            </div>
+            <div class="row mt-5 gap-3">
+
+                <div class="col">
+                    <h5 class="h5">مقالات پر بازدید</h5>
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th>Firstname</th>
+                            <th>Lastname</th>
+                            <th>Email</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>John</td>
+                            <td>Doe</td>
+                            <td>john@example.com</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="col">
+                    <h5>بیشترین پست های نظر داده شده</h5>
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th>Firstname</th>
+                            <th>Lastname</th>
+                            <th>Email</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>John</td>
+                            <td>Doe</td>
+                            <td>john@example.com</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="col">
+                    <h5>دیدگا ها</h5>
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th>Firstname</th>
+                            <th>Lastname</th>
+                            <th>Email</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>John</td>
+                            <td>Doe</td>
+                            <td>john@example.com</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+
             </div>
 
         </div>
