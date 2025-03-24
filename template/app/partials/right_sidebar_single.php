958ec919b1ca675738fb2d1264b9a7dabb09ec53
mason.hows11 <mason.hows11@gmail.com>
2025-03-24 07:46:17 +0330

    add single article for front template

---
+++ template/app/partials/right_sidebar_single.php
@@ -0,0 +1,120 @@
+<div class="col-lg-4">
+    <div class="top-sticky-wrapper">
+        <div class="widget-area">
+            <div class="widget widget_search">
+                <h2 class="widget-headline">Search</h2>
+                <form action="blog.html" class="search-form">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Search ...">
+                    </div>
+                    <button class="submit-btn" type="submit"><i class="fa fa-search"></i></button>
+                </form>
+            </div>
+            <div class="widget about_me_widget">
+                <h2 class="widget-headline">About Me</h2>
+                <div class="content">
+                    <div class="thumb">
+                        <img src="assets/img/section-img/business/business-clinet-02.png" alt="">
+                    </div>
+                    <p>I love to travel and then share my emotions with the readers. Thank you for following me on social media. Stay tuned for more updates!</p>
+                    <div class="signature">
+                        <img src="assets/img/section-img/business/Signature.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="widget widget_social">
+                <h3 class="widget-headline">Follow us<span></span></h3>
+                <ul class="contact_info_list-02">
+                    <li class="single-info-item-03">
+                        <div class="icon">
+                            <i class="fab fa-facebook-square"></i>
+                        </div>
+                        <div class="details">
+                            <p><span>1250M +</span>Followers</p>
+                        </div>
+                    </li>
+                    <li class="single-info-item-03">
+                        <div class="icon style-01">
+                            <i class="fab fa-twitter-square"></i>
+                        </div>
+                        <div class="details">
+                            <p><span>1250M +</span>Followers</p>
+                        </div>
+                    </li>
+                    <li class="single-info-item-03">
+                        <div class="icon style-02">
+                            <i class="fab fa-youtube-square"></i>
+                        </div>
+                        <div class="details">
+                            <p><span>1250M +</span>Followers</p>
+                        </div>
+                    </li>
+                    <li class="single-info-item-03">
+                        <div class="icon style-03">
+                            <i class="fab fa-linkedin"></i>
+                        </div>
+                        <div class="details">
+                            <p><span>1250M +</span>Followers</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="widget widget_newsim_category-02">
+                <h3 class="widget-headline">Categories</h3>
+                <ul>
+                    <li style="background-image: url(assets/img/section-img/business/domain.png);"><a href="#">Domain<span>05</span></a></li>
+                    <li style="background-image: url(assets/img/section-img/business/city-news.png);"><a href="#">City News<span>05</span></a></li>
+                    <li style="background-image: url(assets/img/section-img/business/domain-02.png);"><a href="#">domain check<span>05</span></a></li>
+                    <li style="background-image: url(assets/img/section-img/business/map.png);"><a href="#">Amazon Map<span>05</span></a></li>
+                </ul>
+            </div>
+            <div class="widget widget_recent_posts">
+                <h4 class="widget-headline">Recent Post</h4>
+                <ul class="theme-recent-post-wrap">
+                    <li class="theme-recent-post-item">
+                        <div class="thumb">
+                            <img src="assets/img/section-img/recent/business-recent.png" alt="recent post">
+                        </div>
+                        <div class="content">
+                            <h4 class="title"><a href="business-02.html">the best beach hikes  the west coast</a></h4>
+                            <span class="time">
+                                                    BY ADMIN
+                                                    <i class="fas fa-calendar-alt"></i>
+                                                    JULY 1, 2022
+                                                </span>
+                        </div>
+                    </li>
+                    <li class="theme-recent-post-item">
+                        <div class="thumb">
+                            <img src="assets/img/section-img/recent/business-recent-02.png" alt="recent post">
+                        </div>
+                        <div class="content">
+                            <h4 class="title"><a href="business-02.html">Freehand Template built for the whole</a></h4>
+                            <span class="time">
+                                                    BY ADMIN
+                                                    <i class="fas fa-calendar-alt"></i>
+                                                    JULY 1, 2022
+                                                </span>
+                        </div>
+                    </li>
+                    <li class="theme-recent-post-item">
+                        <div class="thumb">
+                            <img src="assets/img/section-img/recent/business-renent-03.png" alt="recent post">
+                        </div>
+                        <div class="content">
+                            <h4 class="title"><a href="business-02.html">Security isn’t just a logy problem it’s</a></h4>
+                            <span class="time">
+                                                    BY ADMIN
+                                                    <i class="fas fa-calendar-alt"></i>
+                                                    JULY 1, 2022
+                                                </span>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="advertise">
+                <img src="assets/img/section-img/business/yotta.png" alt="">
+            </div>
+        </div>
+    </div>
+</div>
