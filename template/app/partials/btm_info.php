47dd24414292bde5d33278850c7f69c71e5b4f7c
mason.hows11 <mason.hows11@gmail.com>
2025-03-24 16:09:20 +0330

    index template updated

---
+++ template/app/partials/btm_info.php
@@ -0,0 +1,168 @@
+<section class="latest-videos-section">
+    <div class="container custom-container-02">
+
+        <div class="theme-heading-title">
+            <div class="title">Latest Videos</div>
+        </div>
+
+        <div class="video-slider-wrap">
+            <div class="video-slider-one">
+                <div class="image-box-item-five bg-image-02" style="background-image: url(../../public/front_assets/img/section-img/videos/videos-01.png);">
+                    <div class="content style-01">
+                        <h4 class="title"><a href="business-02.html">Disney World Just Toyed With Legions of Superfans</a></h4>
+                        <ul class="post-meta">
+                            <li class="white">
+                                <a href="#">By <span class="style-01">Admin</span></a>
+                            </li>
+                            <li>
+                                        <span class="posted-on">
+                                            <a href="#" rel="bookmark">
+                                                <span class="entry-date white published updated"><i class="fas fa-calendar-alt"></i>JAN 14, 2022</span>
+                                            </a>
+                                        </span>
+                            </li>
+                        </ul>
+                    </div>
+                    <a class="video-play-btn mfp-iframe" href="https://www.youtube.com/watch?v=c7XEhXZ_rsk" tabindex="0">
+                        <i class="fas fa-play"></i>
+                    </a>
+                </div>
+                <div class="image-box-item-five bg-image-02" style="background-image: url(../../public/front_assets/img/section-img/videos/videos-02.png);">
+                    <div class="content style-01">
+                        <h4 class="title"><a href="business-02.html">Disney World Just Toyed With Legions of Superfans</a></h4>
+                        <ul class="post-meta">
+                            <li class="white">
+                                <a href="#">By <span class="style-01">Admin</span></a>
+                            </li>
+                            <li>
+                                        <span class="posted-on">
+                                            <a href="#" rel="bookmark">
+                                                <span class="entry-date white published updated"><i class="fas fa-calendar-alt"></i>JAN 14, 2022</span>
+                                            </a>
+                                        </span>
+                            </li>
+                        </ul>
+                    </div>
+                    <a class="video-play-btn mfp-iframe" href="https://www.youtube.com/watch?v=c7XEhXZ_rsk" tabindex="0">
+                        <i class="fas fa-play"></i>
+                    </a>
+                </div>
+                <div class="image-box-item-five bg-image-02" style="background-image: url(../../public/front_assets/img/section-img/videos/videos-03.png);">
+                    <div class="content style-01">
+                        <h4 class="title"><a href="business-02.html">Disney World Just Toyed With Legions of Superfans</a></h4>
+                        <ul class="post-meta">
+                            <li class="white">
+                                <a href="#">By <span class="style-01">Admin</span></a>
+                            </li>
+                            <li>
+                                        <span class="posted-on">
+                                            <a href="#" rel="bookmark">
+                                                <span class="entry-date white published updated"><i class="fas fa-calendar-alt"></i>JAN 14, 2022</span>
+                                            </a>
+                                        </span>
+                            </li>
+                        </ul>
+                    </div>
+                    <a class="video-play-btn mfp-iframe" href="https://www.youtube.com/watch?v=c7XEhXZ_rsk" tabindex="0">
+                        <i class="fas fa-play"></i>
+                    </a>
+                </div>
+                <div class="image-box-item-five bg-image-02" style="background-image: url(../../public/front_assets/img/section-img/videos/videos-04.png);">
+                    <div class="content style-01">
+                        <h4 class="title"><a href="business-02.html">Disney World Just Toyed With Legions of Superfans</a></h4>
+                        <ul class="post-meta">
+                            <li class="white">
+                                <a href="#">By <span class="style-01">Admin</span></a>
+                            </li>
+                            <li>
+                                        <span class="posted-on">
+                                            <a href="#" rel="bookmark">
+                                                <span class="entry-date white published updated"><i class="fas fa-calendar-alt"></i>JAN 14, 2022</span>
+                                            </a>
+                                        </span>
+                            </li>
+                        </ul>
+                    </div>
+                    <a class="video-play-btn mfp-iframe" href="https://www.youtube.com/watch?v=c7XEhXZ_rsk" tabindex="0">
+                        <i class="fas fa-play"></i>
+                    </a>
+                </div>
+                <div class="image-box-item-five bg-image-02" style="background-image: url(../../public/front_assets/img/section-img/videos/videos-02.png);">
+                    <div class="content style-01">
+                        <h4 class="title"><a href="business-02.html">Disney World Just Toyed With Legions of Superfans</a></h4>
+                        <ul class="post-meta">
+                            <li class="white">
+                                <a href="#">By <span class="style-01">Admin</span></a>
+                            </li>
+                            <li>
+                                        <span class="posted-on">
+                                            <a href="#" rel="bookmark">
+                                                <span class="entry-date white published updated"><i class="fas fa-calendar-alt"></i>JAN 14, 2022</span>
+                                            </a>
+                                        </span>
+                            </li>
+                        </ul>
+                    </div>
+                    <a class="video-play-btn mfp-iframe" href="https://www.youtube.com/watch?v=c7XEhXZ_rsk" tabindex="0">
+                        <i class="fas fa-play"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="call-to-section-area style-01 bg-image" style="background-image: url(../../public/front_assets/img/section-img/cta/cta-bg.png);">
+    <div class="container custom-container-02">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="image-box-item style-01">
+                    <div class="thumbnail">
+                        <img src="../../public/front_assets/img/section-img/cta/cta-01.png" alt="">
+                    </div>
+                    <div class="content">
+                        <span class="tag-02">LIFE STYLE</span>
+                        <a href="business-02.html"><h4 class="title">Ducati ‘has all the cards’ to win The Best Ways to Start</h4></a>
+                        <p>The Best Places to Begin The Best Places to Begin The Best Places to Begin The Best Places to Begin The Best</p>
+                        <ul class="post-meta">
+                            <li>
+                                <a href="#">By <span>Admin</span></a>
+                            </li>
+                            <li>
+                                        <span class="posted-on">
+                                            <a href="#" rel="bookmark">
+                                                <span class="entry-date published updated"><i class="fas fa-calendar-alt"></i>JAN 14, 2022</span>
+                                            </a>
+                                        </span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="image-box-item style-01">
+                    <div class="thumbnail">
+                        <img src="../../public/front_assets/img/section-img/cta/cta-02.png" alt="">
+                    </div>
+                    <div class="content">
+                        <span class="tag-02">FASHION</span>
+                        <a href="business-02.html"><h4 class="title">Moon They Landing How are Main Close If you went</h4></a>
+                        <p>The Best Places to Begin The Best Places to Begin The Best Places to Begin The Best Places to Begin The Best</p>
+                        <ul class="post-meta">
+                            <li>
+                                <a href="#">By <span>Admin</span></a>
+                            </li>
+                            <li>
+                                        <span class="posted-on">
+                                            <a href="#" rel="bookmark">
+                                                <span class="entry-date published updated"><i class="fas fa-calendar-alt"></i>JAN 14, 2022</span>
+                                            </a>
+                                        </span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
