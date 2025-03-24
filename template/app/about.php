1c321fd5a10f2a31d43a12ca80143699ee08f368
mason.hows11 <mason.hows11@gmail.com>
2025-03-24 09:06:18 +0330

    updated

--- template/app/about.php
+++ template/app/about.php
@@ -49,9 +49,9 @@
                                     <li><a href="<?php echo url('about_us')?>">About Us</a></li>
                                 </ul>
                             </div>
-                            <div class="advertise-banner">
+                            <!-- <div class="advertise-banner">
                                 <img src="assets/img/section-img/advertise/advertise-bg-01.png" alt="">
-                            </div>
+                            </div> -->
                         </div>
                     </div>
                 </div>
@@ -88,104 +88,9 @@
                 </div>
             </div>
         </section>
-        <!-- Author Section End Here -->
-
-
-
-        <!-- Team Section Start Here -->
-      <!--  <section class="team-section-area">
-            <div class="container custom-container">
-                <div class="theme-heading-title desktop-center">
-                    <h6 class="subtitle">Our Team</h6>
-                    <h4 class="title">Meet Our Team</h4>
-                </div>
-                <div class="row">
-                    <div class="col-lg-4 col-md-6">
-                        <div class="team-single-item">
-                            <div class="thumb">
-                                <img src="assets/img/section-img/team/team-01.png" alt="">
-                                <ul class="social-icon style-01">
-                                    <li><a href="#" class="style-01" tabindex="0"><i class="fab fa-facebook-f"></i></a></li>
-                                    <li><a href="#" class="style-01" tabindex="0"><i class="fab fa-twitter"></i></a></li>
-                                    <li><a href="#" class="style-01" tabindex="0"><i class="fab fa-linkedin-in"></i></a></li>
-                                    <li><a href="#" class="style-01" tabindex="0"><i class="fab fa-instagram"></i></a></li>
-                                </ul>
-                            </div>
-
-                            <div class="content">
-                                <a href="#" tabindex="0"><h2 class="title">Kelvine Pitterson</h2></a>
-                                <span>Sr. Marketer</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6">
-                        <div class="team-single-item">
-                            <div class="thumb">
-                                <img src="assets/img/section-img/team/team-02.png" alt="">
-                                <ul class="social-icon style-01">
-                                    <li><a href="#" class="style-01" tabindex="0"><i class="fab fa-facebook-f"></i></a></li>
-                                    <li><a href="#" class="style-01" tabindex="0"><i class="fab fa-twitter"></i></a></li>
-                                    <li><a href="#" class="style-01" tabindex="0"><i class="fab fa-linkedin-in"></i></a></li>
-                                    <li><a href="#" class="style-01" tabindex="0"><i class="fab fa-instagram"></i></a></li>
-                                </ul>
-                            </div>
-
-                            <div class="content">
-                                <a href="#" tabindex="0"><h2 class="title">Abhisek Roy</h2></a>
-                                <span>News Editor</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6">
-                        <div class="team-single-item">
-                            <div class="thumb">
-                                <img src="assets/img/section-img/team/team-03.png" alt="">
-                                <ul class="social-icon style-01">
-                                    <li><a href="#" class="style-01" tabindex="0"><i class="fab fa-facebook-f"></i></a></li>
-                                    <li><a href="#" class="style-01" tabindex="0"><i class="fab fa-twitter"></i></a></li>
-                                    <li><a href="#" class="style-01" tabindex="0"><i class="fab fa-linkedin-in"></i></a></li>
-                                    <li><a href="#" class="style-01" tabindex="0"><i class="fab fa-instagram"></i></a></li>
-                                </ul>
-                            </div>
-
-                            <div class="content">
-                                <a href="#" tabindex="0"><h2 class="title">Rashma Mandan</h2></a>
-                                <span>News Presentar</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>-->
-        <!-- Team Section End Here -->
-
-        <!-- Newslatter Section Start Here -->
-       <!-- <div class="news-latter-section style-01 bg-image" style="background-image: url(assets/img/section-img/cta/cta-bg.png);">
-            <div class="container custom-container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="subscibe-wrapper">
-                            <div class="content">
-                                <div class="title">Sign Up for Our Newsletter <br> For Daily Update</div>
-                            </div>
-                            <div class="subscribe-form">
-                                <div class="form-group">
-                                    <input type="text" name="fname" placeholder="Enter Email" class="form-control" required="" aria-required="true">
-                                </div>
-                                <div class="btn-wrap">
-                                    <a href="service-single.html" class="boxed-btn subscribe-btn">subscribe now</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>-->
-        <!-- Newslatter Section End Here -->
+     
     </main>
-    <!-- Main Section End Here -->
-
-    <!-- footer area start -->
+ 
 <?php
     include_once BASE_PATH . "/template/app/layouts/footer.php";
 ?>
